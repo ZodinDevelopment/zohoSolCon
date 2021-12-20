23a94a6e11a9a998d4dabf9c5bf27531abffee97
toasterkief <toasterkief@localhost.localdomain>
2021-12-20 07:37:07 -0600

    first commit

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.1'
+VERSION = '0.2'
 DESCRIPTION = "Implementation tools for Zoho CRM"
 LONG_DESCRIPTION = "A pet project I'm working, makes API calls to Zoho APIS"
 
 setup(
-    name="zohoDevKit",
+    name="zohoSolCon",
     version=VERSION,
     author="Dylan Garrett",
     author_email="dylan.g@zohocorp.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    pages=find_packages(),
-    install_requires=['requests', 'json'],
+    packages=find_packages(),
     keywords=['zoho', 'crm', 'api']
 )
