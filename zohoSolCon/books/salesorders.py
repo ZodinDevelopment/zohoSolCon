ba4f6910c6c2e3f1db987ae4ec4c5ad360cef5d2
Dylan Garrett <dmgarrett72@protonmail.com>
2022-03-07 00:42:32 -0600

    $ Books Additions 	* invoices module underway $ NEW Module: Creator API

--- zohoSolCon/books/salesorders.py
+++ zohoSolCon/books/salesorders.py
@@ -116,15 +116,14 @@ def mark_status(token, org_id, salesorder_id, status="open"):
         return token, content.get("message")
 
 
-def email_salesorder(token, org_id, salesorder_id, to_mail_ids, subject, body, **kwargs):
+def email_salesorder(token, org_id, salesorder_id, to_mail_ids, **kwargs):
     url = f'https://books.zoho.com/api/v3/salesorders/{salesorder_id}/email'
     headers = make_header(token)
     params = {'organization_id': org_id}
 
     data_object = kwargs
     data_object['to_mail_ids'] = to_mail_ids
-    data_object['body'] = body
-    data_object['subject'] = subject
+
 
     data = json.dumps(data_object).encode('utf-8')
 
@@ -132,7 +131,7 @@ def email_salesorder(token, org_id, salesorder_id, to_mail_ids, subject, body, *
 
     if response.status_code == 401:
         token.generate()
-        return email_salesorder(token, org_id, salesorder_id, to_mail_ids, subject, body, **kwargs)
+        return email_salesorder(token, org_id, salesorder_id, to_mail_ids,**kwargs)
 
     else:
         content = json.loads(response.content.decode('utf-8'))
@@ -158,4 +157,105 @@ def get_salesorder_email_content(token, org_id, salesorder_id, **kwargs):
 
 
 def submit_for_approval(token, org_id, salesorder_id):
-    url = f'https://books.zoho.com/api/v3/salesorders
+    url = f'https://books.zoho.com/api/v3/salesorders/{salesorder_id}/submit'
+    headers = make_header(token)
+    params = {'organization_id': org_id}
+
+    response = requests.post(url=url, headers=headers, params=params)
+
+    if response.status_code == 401:
+        token.generate()
+        return submit_for_approval(token, org_id, salesorder_id)
+
+    else:
+        content = json.loads(response.content.decode('utf-8'))
+        return token, content.get('message')
+
+
+def approve_salesorder(token, org_id, salesorder_id):
+    url = f'https://books.zoho.com/api/v3/salesorders/{salesorder_id}/approve'
+    headers = make_header(token)
+    params = {'organization_id': org_id}
+
+    response = requests.post(url=url, headers=headers, params=params)
+
+    if response.status_code == 401:
+        token.generate()
+        return approve_salesorder(token, org_id, salesorder_id)
+
+    else:
+        content = json.loads(response.content.decode('utf-8'))
+        return token, content.get("message")
+
+
+def update_billing_address(token, org_id, salesorder_id, address_object):
+    url = f'https://books.zoho.com/api/v3/salesorders/{salesorder_id}/address/billing'
+    headers = make_header(token)
+    params = {'organization_id': org_id}
+
+    data = json.dumps(address_object).encode('utf-8')
+
+    response = requests.put(url=url, headers=headers, params=params, data=data)
+
+    if response.status_code == 401:
+        token.generate()
+        return update_billing_address(token, org_id, salesorder_id, address_object)
+
+    else:
+        content = json.loads(response.content.decode('utf-8'))
+        return token, content.get("message")
+
+
+def update_shipping_address(token, org_id, salesorder_id, address_object):
+    url = f'https://books.zoho.com/api/v3/salesorders/{salesorder_id}/address/shipping'
+    headers = make_header(token)
+    params = {'organization_id': org_id}
+
+    data = json.dumps(address_object).encode('utf-8')
+
+    response = requests.put(url=url, headers=headers, params=params, data=data)
+
+    if response.status_code == 401:
+        token.generate()
+        return update_shipping_address(token, org_id, salesorder_id, address_object)
+
+    else:
+        content = json.loads(response.content.decode('utf-8'))
+        return token, content.get("message")
+
+
+def get_salesorder_templates(token, org_id):
+    url = 'https://books.zoho.com/api/v3/salesorders/templates'
+    headers = make_header(token)
+    params = {'organization_id': org_id}
+
+    response = requests.get(url=url, headers=headers, params=params)
+
+    if response.status_code == 401:
+        token.generate()
+        return get_salesorder_templates(token, org_id)
+
+    else:
+        content = json.loads(response.content.decode('utf-8'))
+        return token, content.get("templates")
+
+
+def update_salesorder_template(token, org_id, salesorder_id, template_id):
+    url = f'https://books.zoho.com/api/v3/salesorders/{salesorder_id}/templates/{template_id}'
+    headers = make_header(token)
+    params = {'organization_id': org_id}
+
+    response = requests.put(url=url, headers=headers, params=params)
+
+    if response.status_code == 401:
+        token.generate()
+        return update_salesorder_template(token, org_id, salesorder_id, template_id)
+
+    else:
+        content = json.loads(response.content.decode('utf-8'))
+        return token, content.get("message")
+
+
+
+
+    
