5f043709727c4bc47da147ca8a45336b37315112
Dylan Garrett <dmgarrett72@protonmail.com>
2022-09-29 05:19:38 -0500

    stilllll

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.4.6_8'
+VERSION = '0.4.6_9'
 DESCRIPTION = "Implementation tools for Zoho CRM"
 LONG_DESCRIPTION = "A pet project I'm working, makes API calls to Zoho APIS"
 
